ae123bfc628702a2485b31cb35ac6a77e5829b40
Marie Lowzow Morland <marie@morland.no>
2021-02-17 23:17:37 +0100

    get data from APIs, build HTML, create error-message, loading-indicator, and finally ended up with a tabel with weather info for the next 24 hours for a municipality of your choice.

--- js/details.js
+++ js/details.js
@@ -1,17 +1,20 @@
 const urlMunicipalityInfoApi = "https://ws.geonorge.no/kommuneinfo/v1/kommuner/";
 const urlWeatherApi= "https://api.met.no/weatherapi/locationforecast/2.0/complete?"
 
+const oneHourInMilliSeconds = 1000 * 60 * 60 * 24;
 const loading = '<div class="loading"></div>';
 
-const noMunicipalityErrormessage = (municipalityNumber) => `<div class="errorMessage">Something went wrong... Could not find municipality number ${municipalityNumber}.</div>`;
-const noWeatherErrormessage = (municipalityName) => `<div class="errorMessage">Something went wrong... Could not find weather information for ${municipalityName}.</div>`;
+const noMunicipalityErrormessage = (municipalityNumber) => 
+    `<div class="errorMessage">
+        Something went wrong... Could not find municipality number ${municipalityNumber}.
+    </div>`;
+
+const noWeatherErrormessage = (municipalityName) => 
+    `<div class="errorMessage">
+        Something went wrong... Could not find weather information for ${municipalityName}.
+    </div>`;
 
 const addToDOM = (html) => document.getElementById("municipality-weather").innerHTML = html;
-
-const findMunicipalityNumber = () => {
-    const search = location.search; 
-    return search.substr(search.indexOf("=") + 1);
-}
 
 const showMunicipality = async () => {
     const municipalityNumber = findMunicipalityNumber();
@@ -24,27 +27,28 @@ const showMunicipality = async () => {
     try {
         const response = await fetch(`${urlMunicipalityInfoApi}${municipalityNumber}`);
         const municipality = await response.json();
-        const {fylkesnavn, kommunenavn, punktIOmrade} = municipality;
-        addToDOM(`
-            <h2>${kommunenavn}</h2>
-            <p>Municipality number: <span class="bold">${municipalityNumber}</span></p>
-            <p>County: <span class="bold">${fylkesnavn}</span></p>
-            ${await getWeatherForCoordinates(punktIOmrade.coordinates, kommunenavn)}`
-        );
+        const municipalityInfo = await municipalityToHtml(municipality);
+        addToDOM(municipalityInfo);
     } catch (error) {
         console.log(error);
         addToDOM(noMunicipalityErrormessage(municipalityNumber));
     }
 };
 
-const weatherTimeseriesHtmlTableRow = (timeserie) => {
-    const {air_temperature, wind_speed, } = timeserie.data.instant.details;
-return `<tr>
-    <td>${timeserie.time}</td>
-    <td>???</td>
-    <td>${air_temperature}</td>
-    <td>${wind_speed}</td>
-</tr>`
+const findMunicipalityNumber = () => {
+    const search = location.search;
+    return search.substr(search.indexOf("=") + 1);
+}
+
+const municipalityToHtml = async (municipality) => {
+    const {fylkesnavn: countyName, kommunenavn: municipalityName, punktIOmrade: point, kommunenummer: municipalityNumber} = municipality;
+    
+    return `
+        <h2>${municipalityName}</h2>
+        <p>Municipality number: <span class="bold">${municipalityNumber}</span></p>
+        <p>County: <span class="bold">${countyName}</span></p>
+        ${await getWeatherForCoordinates(point.coordinates, municipalityName)}
+    `;
 };
 
 const getWeatherForCoordinates = async (coordinates, municipalityName) => {
@@ -52,28 +56,60 @@ const getWeatherForCoordinates = async (coordinates, municipalityName) => {
         const [lon, lat] = coordinates;
         const response = await fetch(`${urlWeatherApi}lon=${lon}&lat=${lat}`);
         const weatherInfo = await response.json();
-        
-        return `
-        <table>
-            <thead>
-                <th>Time</th>
-                <th>Weather</th>
-                <th>Temperature</th>
-                <th>Wind</th>
-            </thead>
-            <tbody>
-                ${weatherInfo.properties.timeseries
-                    .map(weatherTimeseriesHtmlTableRow)
-                    .join("")
-                }
-            </tbody>
-        </table>`
+        return weatherInfoToHtml(weatherInfo);
     } catch (error) {
         console.log(error);
         return noWeatherErrormessage(municipalityName);
     }
 };
 
+const weatherInfoToHtml = (weatherInfo) => {
+    const {timeseries, meta} = weatherInfo.properties;
+    const {precipitation_amount, probability_of_precipitation, air_temperature, wind_speed} = meta.units;
+    
+    return `
+        <table>
+            <thead>
+                <th>Time</th>
+                <th>Precipitation Amount (${precipitation_amount})</th>
+                <th>Probability of precipitation (${probability_of_precipitation})</th>
+                <th>Air temperature (${air_temperature})</th>
+                <th>Wind (${wind_speed})</th>
+            </thead>
+            <tbody>
+                ${timeseries
+                    .filter(next24hours)
+                    .map(weatherTimeseriesHtmlTableRow)
+                    .join("")
+                }
+            </tbody>
+        </table>`;
+} 
+
+const next24hours = (timeserie) =>{
+    const weatherDataTime = new Date(timeserie.time);
+    const now = new Date();
+    return (weatherDataTime - now) < oneHourInMilliSeconds;
+};
+
+const weatherTimeseriesHtmlTableRow = (timeserie) => {
+    const {instant, next_1_hours, } = timeserie.data;
+    const {air_temperature, wind_speed, } = instant.details;
+    const {precipitation_amount, probability_of_precipitation } = next_1_hours && next_1_hours.details || {};
+
+    return `<tr>
+        <td>${new Date(timeserie.time).toLocaleString()}</td>
+        <td>${precipitation_amount || ""}</td>
+        <td>${probability_of_precipitation || ""}</td>
+        <td>${air_temperature}</td>
+        <td>${wind_speed}</td>
+    </tr>`
+};
+
 showMunicipality();
 
 
+/*
+https://stackoverflow.com/questions/1226714/how-to-get-the-browser-to-navigate-to-url-in-javascript
+https://wesbos.com/destructuring-renaming
+*/
--- js/script.js
+++ js/script.js
@@ -1,8 +1,50 @@
-const urlMunicipalities = "https://ws.geonorge.no/kommuneinfo/v1/fylkerkommuner";
+const urlMunicipalityInfoApi = "https://ws.geonorge.no/kommuneinfo/v1/fylkerkommuner";
 
-const municipalityAnchorTag = (municipalityName) => `<a href="/character.html?municipality_name=${municipalityName}">${municipalityName}</a>`
+const loading = '<div class="loading"></div>';
+const errormessage = '<div class="errorMessage">Something went wrong... Try again later.</div>';
 
-document.getElementById("options").onchange = (event) => {
-    const kommunenummer = event.target.value;
-    window.location.href = "/details.html?id=" + kommunenummer;
+const addToDOM = (html) => document.getElementById("options").innerHTML = html;
+
+const showMunicipalityOptions = async () => {
+    addToDOM(loading);
+    
+    try {
+        const response = await fetch(urlMunicipalityInfoApi);
+        const counties = await response.json();
+        addToDOM(countiesToOptions(counties));
+        subscribeToChange();
+    } catch (error) {
+        console.log(error);
+        addToDOM(errormessage);
+    }
+};
+
+const countiesToOptions = (counties) => `
+    <label for="municipality-slector">Your municipality:</label>
+    <input list="municipality-list" id="municipality-slector" placeholder="Choose a municipality"/>
+    <datalist name="municipality" id="municipality-list">
+        ${counties.flatMap(countyToOptions).join("")}
+    </datalist>`;
+
+const countyToOptions = (county) => county.kommuner.map(municipalityToOption)
+
+const municipalityToOption = (municipality) => {
+    const {fylkesnavn: countyName, kommunenavn: municipalityName, kommunenummer: municipalityNumber} = municipality;
+    return `
+        <option value=${municipalityNumber}>
+            ${municipalityName}, ${countyName}
+        </option>`;
 }
+
+const subscribeToChange = () =>
+    document.getElementById("municipality-slector").onchange = (event) => {
+        const kommunenummer = event.target.value;
+        window.location.href = "/details.html?municipalityNumber=" + kommunenummer;
+    }
+
+
+showMunicipalityOptions()
+
+/*
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flatMap
+*/
